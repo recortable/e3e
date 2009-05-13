010a4b33df031ceffa953260fe7a4d38787983f3
dani <dani@mandarine.(none)>
2009-05-13 20:02:42 +0200

    automatic survey creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,12 @@
 class User < ActiveRecord::Base
+  has_one :survey
   acts_as_authentic
-
   validates_presence_of :question, :answer
+
+  after_create :create_survey
+
+  private
+  def create_survey
+    Survey.create!(:user_id => self.id)
+  end
 end
