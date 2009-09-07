59c4a6a11158e5772e5c914458e6a4a0b1fb0675
dani <dani@mandarine.(none)>
2009-09-07 13:12:46 +0200

    minor changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
 
-  map.root :controller => 'about'
+  map.root :controller => 'about', :action => 'what'
 
   map.login "/entrar", :controller => "user_sessions", :action => "new"
   map.logout "/salir", :controller => "user_sessions", :action => "destroy"
