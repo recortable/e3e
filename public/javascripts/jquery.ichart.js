47fecbdcbd4fb43b1a3a62f8d8301e5081c520be
dani <danigb@gmail.com>
2009-09-29 17:22:38 +0200

    chart

--- public/javascripts/jquery.ichart.js
+++ public/javascripts/jquery.ichart.js
@@ -28,17 +28,15 @@
             padding: {
                 top: 20,
                 bottom: 20,
-                left: 30,
-                right: 30
-            },
-            space: {
-                vertical: 10
+                left: 50,
+                right: 10
             }
         },
         yaxis : {
             name: 'Yaxis',
             min: 0,
-            max: 4000
+            max: 4000,
+            division: 500
         },
         xaxis : {
             name: 'Xaxis'
@@ -57,35 +55,54 @@
     }
 
     function redraw(chart) {
-        //     drawGrid(chart);
+        chart.grid.ratio = chart.grid.height / chart.yaxis.max;
+        reset(chart.ctx);
+        drawGrid(chart);
         drawBars(chart);
+        drawLabels(chart);
+    }
+
+    function reset(ctx) {
+        ctx.shadowOffsetX = 0;
+        ctx.shadowOffsetY = 0;
+        ctx.lineWidth = 0.2;
+        ctx.shadowBlur = 0;
     }
 
     function drawGrid(chart) {
         var ctx = chart.ctx;
-        for (var y=chart.grid.padding.top; y < chart.grid.height; y += chart.grid.space.vertical) {
-            rect(ctx, chart.grid.padding.left, y, chart.grid.width, 1);
-        }
 
-        ctx.fillText(chart.xaxis.name, 120, 30)
-        ctx.rotate(- Math.PI*2 / 4);
-        ctx.fillText(chart.yaxis.name, -120, 30);
-        ctx.restore();
+        ctx.strokeStyle = "gray";
+        ctx.strokeRect(chart.grid.padding.left, chart.grid.padding.top, chart.grid.width, chart.grid.height);
+
+        var vspace = chart.yaxis.division * chart.grid.ratio;
+        var max = chart.grid.height;
+        for (var y = chart.grid.padding.top + vspace; y < max; y += vspace) {
+            ctx.strokeRect(chart.grid.padding.left, y, chart.grid.width, 0.1);
+        }
     }
 
     function drawBars(chart) {
         var xOffset = chart.bars.width + chart.bars.margin.left + chart.bars.margin.right;
         var yOffset = chart.grid.height + chart.grid.padding.top;
         chart.ctx.fillStyle = chart.bars.color;
-        var x = chart.grid.padding.left;
+        var x = chart.grid.padding.left + chart.bars.margin.left;
         
         var values = chart.data.values;
         var max = values.length
         for(var index = 0 ; index < max; index++) {
-            var height = chart.grid.height * (values[index] / chart.yaxis.max)
+            var height = values[index] * chart.grid.ratio;
             rect(chart.ctx, x, yOffset - height, chart.bars.width, height);
             x += xOffset;
         }
+    }
+
+    function drawLabels(chart) {
+        var ctx = chart.ctx;
+        ctx.fillText(chart.xaxis.name, 120, 30)
+        ctx.rotate(- Math.PI*2 / 4);
+        ctx.fillText(chart.yaxis.name, -120, 30);
+        ctx.restore();
     }
 
     // LOG
